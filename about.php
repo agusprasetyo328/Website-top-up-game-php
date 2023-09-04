35036e62bd3397ff9e6873203737d5af017423be
Agus Prasetyo <agusprasetyo381328@gmail.com>
2023-09-04 12:03:22 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,76 @@
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>infinityshop</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.cdnfonts.com/css/games" rel="stylesheet">
+</head>
+<body style="overflow-x:hidden; padding-bottom:100px;">
+  <?php
+  include 'includes/header_menu.php';
+  ?>
+  <div>
+    <div class="container mt-5 ">
+      <div class="row justify-content-around">
+        <div class="col-md-5 mt-3">
+          <h3 class="text-warning pt-3 title">Hubungi kami kapan saja</h3>
+          <hr />
+          <p class="mt-2">Beberapa layanan mungkin belum optimal, dikarenakan perbaikann website, untuk peningkatan performa, Mohon Maaf atas ketidaknyamanannya</p>
+        </div>
+      </div>
+      <div class="container pb-3">
+      </div>
+      <div class="container mt-3 d-flex justify-content-center card pb-3 col-md-6">
+        <form class="col-md-12" action="https://formspree.io/agusprasetyo381328@gmail.com" method="POST" name="_next">
+          <h3 class="text-warning pt-3 title mx-auto">Contact Form</h3>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email"
+            name="email">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="5"></textarea>
+          </div>
+          <input type="hidden" name="_next" value="http://localhost/foody/about.php" />
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+    <!--footer -->
+    <?php include 'includes/footer.php'?>
+    <!--footer end-->
+  </body>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('[data-toggle="popover"]').popover();
+    });
+    $(document).ready(function () {
+      if (window.location.href.indexOf('#login') != -1) {
+        $('#login').modal('show');
+      }
+    });
+  </script>
+  <?php if(isset($_GET['error'])){ $z=$_GET['error']; echo "<script type='text/javascript'>
+  $(document).ready(function(){
+    $('#signup').modal('show');
+    });
+    </script>"; echo "
+    <script type='text/javascript'>alert('".$z."')</script>";} ?>
+    <?php if(isset($_GET['errorl'])){ $z=$_GET['errorl']; echo "<script type='text/javascript'>
+    $(document).ready(function(){
+      $('#login').modal('show');
+      });
+      </script>"; echo "
+      <script type='text/javascript'>alert('".$z."')</script>";} ?>
+      </html>
---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>infinityshop</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.cdnfonts.com/css/games" rel="stylesheet">
+</head>
+<body style="margin-bottom:200px">
+  <!--Header-->
+  <?php
+  include 'includes/header_menu.php';
+  include 'includes/check-if-added.php';
+  ?>
+  <!--Header ends-->
+  <div id="content">
+    <div id="bg" class=" ">
+      <div class="container" style="padding-top:150px">
+        <div class="mx-auto p-5 text-white" id="banner_content" style="border-radius: 0.5rem;" >
+          <h1>Top Up Game</h1>
+          <p>Beli voucher game secara mudah dan instan. Top up berbagai voucher game online harga murah dengan metode pembayaran yang aman</p>
+          <a href="maintenance.html" class="btn btn-warning btn-lg text-white">Top Up Sekarang</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="text-center pt-4 h3">
+    * Topup Game *
+  </div>
+  <!--menu highlights start-->
+  <div class="container pt-3">
+    <div class="row text-center ">
+      <div class="col-6 col-md-3 py-3">
+        <a href="maintenance.html"> <img src="images/lv_0_20230824085802.gif" class="img-fluid "alt="" style="border-radius:0.5rem">
+          <!-- https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fm=jpg&w=400&fit=max-->
+          <div class="h5 pt-3 font-weight-bolder">
+            Mobile Legends
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-md-3 py-3 " >
+        <a href="maintenance.html">
+          <img src="images/lv_0_20230824085409.gif" class="img-fluid zoom" alt="" style="border-radius:0.5rem" >
+          <div class="h5 pt-3 font-weight-bolder">
+            Free Fire
+          </div>
+        </a>
+      </div>
+      <div class="col-6 col-md-3 py-3">
+        <a href="maintenance.html">
+         <img src="images/lv_0_20230824091700.gif" class="img-fluid   " alt="" style="border-radius:0.5rem">
+         <div class="h5 pt-3 font-weight-bolder">
+          PUBG Mobile
+        </div>
+      </a>
+    </div>
+    <div class="col-6 col-md-3 py-3">
+      <a href="maintenance.html">
+       <img src="images/lv_0_20230824174208.gif" class="img-fluid  " alt="" style="border-radius:0.5rem">
+       <div class="h5 pt-3 font-weight-bolder">
+        Genshin Impact
+      </div>
+    </div>
+  </a>
+</div>
+</div>
+<!--menu highlights end-->
+<!--footer -->
+<?php include 'includes/footer.php'?>
+<!--footer end-->
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script>
+  $(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+  });
+  $(document).ready(function() {
+
+    if(window.location.href.indexOf('#login') != -1) {
+      $('#login').modal('show');
+    }
+  });
+</script>
+<?php if (isset($_GET['error'])) {$z = $_GET['error'];
+echo "<script type='text/javascript'>
+$(document).ready(function(){
+  $('#signup').modal('show');
+  });
+  </script>";
+  echo "<script type='text/javascript'>alert('" . $z . "')</script>";}?>
+  <?php if (isset($_GET['errorl'])) {$z = $_GET['errorl'];
+  echo "<script type='text/javascript'>
+  $(document).ready(function(){
+    $('#login').modal('show');
+    });
+    </script>";
+    echo "<script type='text/javascript'>alert('" . $z . "')</script>";}?>
+    </html>
